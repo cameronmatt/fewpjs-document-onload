57f1400cba1545d15045eb99893c89ea9fc319b0
Matthew <cameron.matt@outlook.com>
2021-03-16 09:56:40 +1100

    document on load done

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', (event) => {
+    console.log('DOM fully loaded and parsed');
+    loadText();
+});
+
+function loadText() {
+    const textContent = "This is really cool!";
+    document.querySelector( 'p' ).innerHTML = textContent;
+};
